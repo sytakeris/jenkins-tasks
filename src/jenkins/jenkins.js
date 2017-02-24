b4866d826b892e5bfd57311e2227ffe89d600909
sytakeris <p998uug@fspa.myntet.se>
2017-02-24 12:45:49 +0200

    Added ability to use jenkins token

--- src/jenkins/jenkins.js
+++ src/jenkins/jenkins.js
@@ -1,11 +1,20 @@
 const url = require("url");
 const querystring = require('querystring');
 const opn = require('opn');
+const fetch = require('node-fetch');
 
 function startTask(task) {
   let parsedUrl = url.parse(task.taskUrl);
   parsedUrl.query = task.params;
-  opn(url.format(parsedUrl));
+
+  if (task.credentials) {
+    let {user, token} = task.credentials;
+    parsedUrl.auth = `${user}:${token}`;
+    fetch(url.format(parsedUrl), {method: "POST"})
+  } else {
+    opn(url.format(parsedUrl));
+  }
+
 }
 
 module.exports = {
